e859856bee2362ad8de1f81320eb97421f1f6e42
ConnerKT <connerthompson121@gmail.com>
2024-07-03 10:38:52 -0500

    refactoring script

--- server/public/script.js
+++ server/public/script.js
@@ -1,4 +1,4 @@
-const socket = new io('ws://localhost:3001');
+const socket = io('ws://localhost:3001');
 
 const msgInput = document.querySelector('#message');
 const nameInput = document.querySelector('#name');
@@ -8,17 +8,16 @@ const userList = document.querySelector('.user-list');
 const roomList = document.querySelector('.room-list');
 const chatDisplay = document.querySelector('.chat-display');
 
-
 function sendMessage(e) {
-    e.preventDefault()
+    e.preventDefault();
     if (nameInput.value && msgInput.value && chatRoom.value) {
         socket.emit('message', {
             name: nameInput.value,
             text: msgInput.value,
-        })
-        msgInput.value = ""
+        });
+        msgInput.value = "";
     }
-    msgInput.focus()
+    msgInput.focus();
 }
 
 function enterRoom(e){
@@ -27,89 +26,76 @@ function enterRoom(e){
         socket.emit('enterRoom', {
             name: nameInput.value,
             room: chatRoom.value
-        })
+        });
     }
 }
 
-document.querySelector('.form-msg')
-    .addEventListener('submit', sendMessage);
-
-document.querySelector('.form-join')
-    .addEventListener('submit', enterRoom);
+document.querySelector('.form-msg').addEventListener('submit', sendMessage);
+document.querySelector('.form-join').addEventListener('submit', enterRoom);
 
 msgInput.addEventListener('keypress', () => {
-        socket.emit('activity', nameInput.value);
+    socket.emit('activity', nameInput.value);
 });
 
-// Listen for messages 
 socket.on("message", (data) => {
-    activity.textContent = ""
+    activity.textContent = "";
     const {name, text, time} = data;
-    const li = document.createElement('li')
-    li.className = 'post'
-    if (name == nameInput.value) li.className = 'post post--left'
-    if (name != nameInput.value && name != ADMIN) li.className = 'post post--right' if (name !== 'Admin') {
-        li.innerHTML = `<div class="post__header ${name === nameInput.value
-            ? 'post__header--user'
-            : 'post__header--reply'
-            }">
+    const li = document.createElement('li');
+    li.className = 'post';
+    li.className = name === nameInput.value ? 'post post--left' : 'post post--right';
+    
+    li.innerHTML = name !== 'Admin' ? 
+    `<div class="post__header ${name === nameInput.value ? 'post__header--user' : 'post__header--reply'}">
         <span class="post__header--name">${name}</span> 
-        <span class="post__header--time">${time}</span> 
-        </div>
-        <div class="post__text">${text}</div>`
-    } else {
-        li.innerHTML = `<div class="post__text">${text}</div>`
-    }
-    document.querySelector('.chat-display').appendChild(li);
+        <span class="post__header--time">${new Date(time).toLocaleTimeString()}</span> 
+     </div>
+     <div class="post__text">${text}</div>` :
+     `<div class="post__text">${text}</div>`;
 
-    chatDisplay.scrollTop = chatDisplay.scrollHeight
+    document.querySelector('.chat-display').appendChild(li);
+    chatDisplay.scrollTop = chatDisplay.scrollHeight;
 });
 
-
-
-let activityTimer
+let activityTimer;
 socket.on("activity", (name) => {
-    activity.textContent = `${name} is typing...`
+    activity.textContent = `${name} is typing...`;
 
-    // Clear after 3 seconds 
-    clearTimeout(activityTimer)
+    clearTimeout(activityTimer);
     activityTimer = setTimeout(() => {
-        activity.textContent = ""
-    }, 3000)
+        activity.textContent = "";
+    }, 3000);
 });
 
 socket.on('userList', ({ users }) => {
-    showUsers(users)
-})
+    showUsers(users);
+});
+
 socket.on('roomList', ({ rooms }) => {
-    showRooms(rooms)
-})
+    showRooms(rooms);
+});
 
 function showUsers(users){
-    usersList.textContent = ''
-    if (users)
-        {
-            usersList.innerHTML = `<em>Users in ${chatRoom.value}: </em>`
-            users.forEach((user, i ) => {
-                usersList.textContent += ` ${user.name}`
-                if (users.length > 1 && i !== users.length -1)
-                    {
-                        usersList.textContent += ','
-                    }
-            })
-        }
+    userList.textContent = '';
+    if (users) {
+        userList.innerHTML = `<em>Users in ${chatRoom.value}: </em>`;
+        users.forEach((user, i) => {
+            userList.textContent += ` ${user.name}`;
+            if (users.length > 1 && i !== users.length -1) {
+                userList.textContent += ',';
+            }
+        });
+    }
 }
+
 function showRooms(rooms){
-    roomList.textContent = ''
-    if (rooms)
-        {
-            roomList.innerHTML = `<em>Active Rooms:</em>`
-            rooms.forEach((room, i ) => {
-                roomList.textContent += ` ${room}`
-                if (rooms.length > 1 && i !== rooms.length -1)
-                    {
-                        roomList.textContent += ','
-                    }
-            })
-        }
+    roomList.textContent = '';
+    if (rooms) {
+        roomList.innerHTML = `<em>Active Rooms:</em>`;
+        rooms.forEach((room, i) => {
+            roomList.textContent += ` ${room}`;
+            if (rooms.length > 1 && i !== rooms.length -1) {
+                roomList.textContent += ',';
+            }
+        });
+    }
 }
